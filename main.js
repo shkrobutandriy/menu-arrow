d2ad236f249cc019f853d5ba71099ff3ed8fa8e5
Andriy Shkrobut <andriy.sshkrobut@gmail.com>
2022-05-30 18:27:00 +0300

    func
    function call

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let marker = document.querySelector("#marker");
 let item = document.querySelectorAll("ul li a");
 
+Indicator(item[0]);
 function Indicator(e) {
   marker.style.top = e.offsetTop + "px";
   marker.style.width = e.offsetWidth + "px";
